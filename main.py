b89a96dedc15a3a3d6a6234c11cbae14f8da2493
ChaitanyaPy <chaitanyapy07@gmail.com>
2021-02-15 08:57:16 +0530

    fixed production bug

--- main.py
+++ main.py
@@ -926,13 +926,17 @@ def test_edit(code):
                     metadata['last_time'] = c_time
                     metadata['last_date'] = c_date
                     f.write(str(metadata))
-                with open('../data/user_data/'+flask.session['username']+'/created_tests/'+code+'.json') as f:
-                    cr_fdata = ast.literal_eval(f.read())
                 try:
-                    with open('../data/response_data/'+code+'.json') as g:
-                        responses_count = len(ast.literal_eval(g.read())['responses'])
+                    with open('../data/user_data/'+flask.session['username']+'/created_tests/'+code+'.json') as f:
+                        cr_fdata = ast.literal_eval(f.read())
+                    try:
+                        with open('../data/response_data/'+code+'.json') as g:
+                            responses_count = len(ast.literal_eval(g.read())['responses'])
+                    except FileNotFoundError:
+                        responses_count = 0
                 except FileNotFoundError:
-                    responses_count = 0
+                    with open('../data/user_data/'+owner+'/created_tests/'+test_id+'.json', 'w') as f:
+                        f.write(str({"last_time": c_time, "last_date": c_date, "name": "Undefined", "subject": "Undefined", "responses_count": 0}))
                 with open('../data/user_data/'+flask.session['username']+'/created_tests/'+code+'.json', 'w') as f:
                     cr_fdata['name'] = n_test_data['test_name']
                     cr_fdata['subject'] = n_test_data['subject']
