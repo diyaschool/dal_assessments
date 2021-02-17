8b6286e90a208787ee9dae6c8dbefc4c83bdb3ac
ChaitanyaPy <chaitanyapy07@gmail.com>
2021-02-17 19:41:57 +0530

    fixed bugs

--- main.py
+++ main.py
@@ -1081,7 +1081,7 @@ def u_r():
 def upload_file(code):
     if flask.request.method == 'GET':
         user_data = get_user_data(flask.session['username'])
-        with open('../data/test_metadata/'+code+'.json')
+        with open('../data/test_metadata/'+code+'.json'):
             test_metadata = ast.literal_eval(f.read())
         if flask.session['username'] != test_metadata['owner']:
             if 'admin' not in user_data['tags'] or 'team' not in user_data['tags'] or 'teacher' not in user_data['tags']:
