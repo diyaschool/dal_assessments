34c46fb8de93c20f48e0ce51add54533688eae8e
ChaitanyaPy <chaitanyapy07@gmail.com>
2021-03-15 23:48:51 +0530

    added password reset interface

--- user_manager.py
+++ user_manager.py
@@ -1,6 +1,7 @@
 import os
 import hashlib
 import shutil
+import getch
 import ast
 
 def create(username, password, name, tags):
@@ -30,33 +31,48 @@ def modify(username, password, name, tags):
 
 def change_password(username, password):
     data = get(username)
-    print(data)
-    print(password)
     with open('../data/user_metadata/'+username, 'w') as f:
         f.write(str({"name": data['name'], "password": hashlib.sha224(password.encode()).hexdigest(), "tags": data['tags']}))
 
+def are_you_sure():
+    while 1:
+        print('Are you sure? [y/n] ')
+        sure = getch.getch().lower()
+        if sure == 'y':
+            return True
+        elif sure == 'n':
+            return False
+        else:
+            print('Input unrecognized.\n')
+
 if __name__ == '__main__':
     while 1:
-        mode = input('Mode? [create/delete/get/modify]: ')
+        mode = input('Mode? [create/delete/change_password]: ')
         if mode == 'create':
             username = input('Username: ')
             password = input('Password: ')
             name = input('Name: ')
             tags = input("Tags: ")
-            tags = tags.split(',')
+            tags = tags.replace(' ', '').split(',')
             create(username, password, name, tags)
-            print('Done.\n')
+            print('Done.')
         elif mode == 'delete':
             username = input('Username: ')
-            sure = input('Are you sure? [y/N]: ').lower()
-            if sure == 'n':
-                print('Not modified.\n')
-            elif sure == 'y':
+            if are_you_sure():
                 delete(username)
             else:
-                print('Not recognized.\n')
+                print('Not modified.')
+        elif mode == 'change_password':
+            username = input('Username: ')
+            new_password = input('New password: ')
+            if are_you_sure():
+                change_password(username, new_password)
+                print('Done.')
+            else:
+                print('Not modified.')
         elif mode == 'fix':
             username = input('Username: ')
             fix(username)
         else:
-            print('Not recognized.\n')
+            print('Not recognized.')
+        print()
